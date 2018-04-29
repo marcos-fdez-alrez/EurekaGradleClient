fbf73dc8b7cd1f6111a67ad12114471fea3ec4e6
marcos-fdez-alrez <marcos.fdez.alrez@gmail.com>
2018-04-29 17:49:10 +0200

    Changes

--- src/main/java/es/nom/marcosfernandez/EurekaGradleClient.java
+++ src/main/java/es/nom/marcosfernandez/EurekaGradleClient.java
@@ -1,22 +1,17 @@
 package es.nom.marcosfernandez;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
-@RestController
+@SpringBootApplication
+@EnableDiscoveryClient
 public class EurekaGradleClient {
-
-    @RequestMapping("/")
-    public String home() {
-        return "Hello world";
-    }
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(EurekaGradleClient.class).web(true).run(args);
---
+++ src/main/java/es/nom/marcosfernandez/services/HelloService.java
@@ -0,0 +1,16 @@
+package es.nom.marcosfernandez.services;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloService {
+
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello world";
+    }
+
+}
